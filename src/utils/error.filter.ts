241e04df6fb9afb12aa96c6cf9c70cdf8105de75
Tanveer-Ali110 <ali.tanveer1012@gmail.com>
2023-12-08 18:42:15 +0500

    error handling

--- src/utils/error.filter.ts
+++ src/utils/error.filter.ts
@@ -8,18 +8,19 @@ import {
 import { Response } from 'express';
 import { MongooseError, mongo } from 'mongoose';
 
+const responseWithError = (response: Response, status: number, error: any) => {
+  return response.status(status).json({ isError: true, error });
+};
+
 @Catch()
 export class GlobalErrorFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
+    const { INTERNAL_SERVER_ERROR, BAD_REQUEST, CONFLICT } = HttpStatus;
     if (exception instanceof BadRequestException) {
-      return responseWithError(
-        response,
-        HttpStatus.BAD_REQUEST,
-        exception.message,
-      );
+      return responseWithError(response, BAD_REQUEST, exception.message);
     }
 
     if (exception instanceof mongo.MongoError) {
@@ -29,31 +30,28 @@ export class GlobalErrorFilter implements ExceptionFilter {
           const errors = Object.keys(keyValue).map((field) => ({
             [field]: `${field.split('_').join(' ')} value already exist`,
           }));
-          return responseWithError(response, HttpStatus.CONFLICT, errors);
+          return responseWithError(response, CONFLICT, errors);
 
         default:
           return responseWithError(
             response,
-            HttpStatus.INTERNAL_SERVER_ERROR,
+            INTERNAL_SERVER_ERROR,
             exception.message,
           );
       }
     }
+
     if (exception instanceof MongooseError) {
       const errors = Object.keys((exception as any).errors).map((field) => ({
         [field]: `${field.split('_').join(' ')} is required`,
       }));
-      return responseWithError(response, HttpStatus.CONFLICT, errors);
+      return responseWithError(response, CONFLICT, errors);
     }
 
     return responseWithError(
       response,
-      HttpStatus.INTERNAL_SERVER_ERROR,
+      INTERNAL_SERVER_ERROR,
       exception.message,
     );
   }
 }
-
-const responseWithError = (response: Response, status: number, error: any) => {
-  return response.status(status).json({ isError: true, error });
-};
